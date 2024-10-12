ee0ac1a19cae070e7e525a25434e25983538cb46
VeriNexus <info@verinexus.com>
2024-10-13 00:55:09 +0100

    Update run_speedtest.sh

--- run_speedtest.sh
+++ run_speedtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Speedtest script version
-RUN_SPEEDTEST_VERSION="1.0.2"
+RUN_SPEEDTEST_VERSION="1.0.3"
 
 run_speed_test() {
     echo -e "${CYAN}┌──────────────────────────────────────────┐${NC}"
@@ -35,4 +35,11 @@ run_speed_test() {
     SPEEDTEST_OUTPUT=$(echo "$SPEEDTEST_OUTPUT" | awk -F, -v date="$UK_DATE" -v time="$UK_TIME" '{OFS=","; print $1, $2, $3, date, time, $6, $7, $8, $9}')
 
     # Upload the result to the remote server
-    echo "$SPEEDTEST_OUTPUT" | sshpass -p "$REMOTE_PASS" ssh -o StrictHostKeyChecking
+    echo "$SPEEDTEST_OUTPUT" | sshpass -p "$REMOTE_PASS" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" "cat >> $REMOTE_PATH"
+
+    if [ $? -eq 0 ]; then
+        echo -e "${CHECKMARK} Results saved to the remote server."
+    else
+        log_error "Failed to save results to the remote server."
+    fi
+}
