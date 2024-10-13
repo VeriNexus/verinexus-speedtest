e9222fd5c6b33cf109aefec3dd53a7d1f6b73619
VeriNexus <info@verinexus.com>
2024-10-13 01:38:34 +0100

    Update run_speedtest.sh

--- run_speedtest.sh
+++ run_speedtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Speedtest script version
-RUN_SPEEDTEST_VERSION="1.0.9"
+RUN_SPEEDTEST_VERSION="1.1.1"
 
 run_speed_test() {
     echo -e "${CYAN}┌──────────────────────────────────────────┐${NC}"
@@ -26,16 +26,17 @@ run_speed_test() {
     UK_TIME=$(TZ="Europe/London" date +"%H:%M:%S")
     echo -e "${CHECKMARK} Date (UK): ${YELLOW}$UK_DATE${NC}, Time (UK): ${YELLOW}$UK_TIME${NC}"
 
-    # Process and upload the results
-    echo -e "${CYAN}┌──────────────────────────────────────────┐${NC}"
-    echo -e "${CYAN}│${NC}  Step 3: Saving Results  ${CYAN}│${NC}"
-    echo -e "${CYAN}└──────────────────────────────────────────┘${NC}"
+    # Clean up special characters in hostname if necessary
+    CLEAN_HOSTNAME=$(hostname | tr -d '[:space:][:punct:]')
+
+    # Prepare the data to write to the file, including speed test output
+    SPEEDTEST_OUTPUT_WITH_METADATA="${SPEEDTEST_OUTPUT},$CLEAN_HOSTNAME,$UK_DATE,$UK_TIME"
 
     # Debug the SSH command
-    echo "Running SSH command: sshpass -p '$REMOTE_PASS' ssh -o StrictHostKeyChecking=no '$REMOTE_USER@$REMOTE_HOST' 'echo \"$SPEEDTEST_OUTPUT\" >> $REMOTE_PATH'"
+    echo "Running SSH command: sshpass -p '$REMOTE_PASS' ssh -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST \"echo '$SPEEDTEST_OUTPUT_WITH_METADATA' >> $REMOTE_PATH\""
 
     # Upload the result to the remote server
-    sshpass -p '$REMOTE_PASS' ssh -o StrictHostKeyChecking=no '$REMOTE_USER@$REMOTE_HOST' "echo '$SPEEDTEST_OUTPUT' >> $REMOTE_PATH"
+    sshpass -p "$REMOTE_PASS" ssh -o StrictHostKeyChecking=no "$REMOTE_USER@$REMOTE_HOST" "echo \"$SPEEDTEST_OUTPUT_WITH_METADATA\" >> \"$REMOTE_PATH\""
     
     if [ $? -eq 0 ]; then
         echo -e "${CHECKMARK} Results saved to the remote server."
