48802936db725c3911b47b7b34364eced3ef75a3
VeriNexus <info@verinexus.com>
2024-10-13 01:43:36 +0100

    Update update_check.sh

--- update_check.sh
+++ update_check.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Update check script version
-UPDATE_CHECK_VERSION="1.0.6"
+UPDATE_CHECK_VERSION="1.1.2"
 
 check_for_updates() {
     # Function to check for script updates
-    LATEST_MAIN_VERSION="1.0.10"
+    LATEST_MAIN_VERSION="1.1.2"
+
+    echo "DEBUG: Running update check..."
+    echo "DEBUG: LATEST_MAIN_VERSION=$LATEST_MAIN_VERSION"
+    echo "DEBUG: SCRIPT_VERSION=$SCRIPT_VERSION"
+
     if [[ "$SCRIPT_VERSION" != "$LATEST_MAIN_VERSION" ]]; then
         echo -e "${YELLOW}Update available for main script: $LATEST_MAIN_VERSION${NC}"
         echo -e "Downloading the latest version..."
@@ -13,5 +18,7 @@ check_for_updates() {
         chmod +x ./speedtest.sh
         echo -e "${GREEN}Update downloaded to version $LATEST_MAIN_VERSION. Please re-run the script.${NC}"
         exit 0
+    else
+        echo -e "${GREEN}You are using the latest version of the script.${NC}"
     fi
 }
