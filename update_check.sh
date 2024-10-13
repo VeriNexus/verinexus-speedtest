e25f2a3b6cc5f47540be801a00eb517bcd8a9b0e
VeriNexus <info@verinexus.com>
2024-10-13 01:28:37 +0100

    Update update_check.sh

--- update_check.sh
+++ update_check.sh
@@ -1,40 +1,17 @@
 #!/bin/bash
 
 # Update check script version
-UPDATE_CHECK_VERSION="1.0.3"
+UPDATE_CHECK_VERSION="1.0.6"
 
-# Initialize version variables for other scripts
-LATEST_MAIN_VERSION=""
-LATEST_ERROR_HANDLER_VERSION=""
-LATEST_UPDATE_CHECK_VERSION=""
-LATEST_RUN_SPEEDTEST_VERSION=""
-LATEST_UTILS_VERSION=""
-
-# Function to check for script updates
 check_for_updates() {
-    echo -e "${CYAN}Checking for updates...${NC}"
-
-    # Fetch the latest version for each component from the local scripts
-    LATEST_MAIN_VERSION=$(grep 'SCRIPT_VERSION' ./speedtest.sh | cut -d'=' -f2 | tr -d '"')
-    LATEST_ERROR_HANDLER_VERSION=$(grep 'ERROR_HANDLER_VERSION' ./error_handler.sh | cut -d'=' -f2 | tr -d '"')
-    LATEST_UPDATE_CHECK_VERSION=$(grep 'UPDATE_CHECK_VERSION' ./update_check.sh | cut -d'=' -f2 | tr -d '"')
-    LATEST_RUN_SPEEDTEST_VERSION=$(grep 'RUN_SPEEDTEST_VERSION' ./run_speedtest.sh | cut -d'=' -f2 | tr -d '"')
-    LATEST_UTILS_VERSION=$(grep 'UTILS_VERSION' ./utils.sh | cut -d'=' -f2 | tr -d '"')
-
-    # Compare versions and inform if an update is available for any component
+    # Function to check for script updates
+    LATEST_MAIN_VERSION="1.0.10"
     if [[ "$SCRIPT_VERSION" != "$LATEST_MAIN_VERSION" ]]; then
         echo -e "${YELLOW}Update available for main script: $LATEST_MAIN_VERSION${NC}"
-    fi
-    if [[ "$ERROR_HANDLER_VERSION" != "$LATEST_ERROR_HANDLER_VERSION" ]]; then
-        echo -e "${YELLOW}Update available for error_handler.sh: $LATEST_ERROR_HANDLER_VERSION${NC}"
-    fi
-    if [[ "$UPDATE_CHECK_VERSION" != "$LATEST_UPDATE_CHECK_VERSION" ]]; then
-        echo -e "${YELLOW}Update available for update_check.sh: $LATEST_UPDATE_CHECK_VERSION${NC}"
-    fi
-    if [[ "$RUN_SPEEDTEST_VERSION" != "$LATEST_RUN_SPEEDTEST_VERSION" ]]; then
-        echo -e "${YELLOW}Update available for run_speedtest.sh: $LATEST_RUN_SPEEDTEST_VERSION${NC}"
-    fi
-    if [[ "$UTILS_VERSION" != "$LATEST_UTILS_VERSION" ]]; then
-        echo -e "${YELLOW}Update available for utils.sh: $LATEST_UTILS_VERSION${NC}"
+        echo -e "Downloading the latest version..."
+        curl -s -o "./speedtest.sh" "https://raw.githubusercontent.com/VeriNexus/verinexus-speedtest/main/speedtest.sh"
+        chmod +x ./speedtest.sh
+        echo -e "${GREEN}Update downloaded to version $LATEST_MAIN_VERSION. Please re-run the script.${NC}"
+        exit 0
     fi
 }
