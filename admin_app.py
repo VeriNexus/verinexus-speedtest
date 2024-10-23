8798f49abde37f29b7273d63c22709e437c92ea3
VeriNexus <info@verinexus.com>
2024-10-23 21:47:19 +0100

    1.1 Admin_App

--- admin_app.py
+++ admin_app.py
@@ -1,3 +1,5 @@
+# Version 1.1
+
 from flask import Flask, request, render_template, redirect, url_for
 from influxdb import InfluxDBClient
 
@@ -8,6 +10,9 @@ INFLUXDB_SERVER = '82.165.7.116'
 INFLUXDB_PORT = 8086
 INFLUXDB_DB = 'test_db'
 INFLUXDB_MEASUREMENT = 'endpoints'
+SPEEDTEST_DB = 'speedtest_db_clean'
+DEVICES_MEASUREMENT = 'devices'
+SPEEDTEST_MEASUREMENT = 'speedtest'
 
 client = InfluxDBClient(host=INFLUXDB_SERVER, port=INFLUXDB_PORT)
 
@@ -69,6 +74,54 @@ def delete_endpoint():
 def drop_database():
     client.drop_database(INFLUXDB_DB)
     return redirect(url_for('index'))
+
+@app.route('/claim', methods=['GET'])
+def claim_device():
+    # Switch to the speedtest_db_clean database
+    client.switch_database(SPEEDTEST_DB)
+
+    # Get all MAC addresses from the speedtest measurement
+    speedtest_query = f'SELECT DISTINCT("tag_mac_address") FROM {SPEEDTEST_MEASUREMENT}'
+    speedtest_result = client.query(speedtest_query)
+    speedtest_macs = {point['distinct'] for point in speedtest_result.get_points()}
+
+    # Get all MAC addresses from the devices measurement
+    devices_query = f'SELECT DISTINCT("tag_mac_address") FROM {DEVICES_MEASUREMENT}'
+    devices_result = client.query(devices_query)
+    devices_macs = {point['distinct'] for point in devices_result.get_points()}
+
+    # Find unclaimed MAC addresses
+    unclaimed_macs = speedtest_macs - devices_macs
+
+    return render_template('claim.html', unclaimed_macs=unclaimed_macs)
+
+@app.route('/claim', methods=['POST'])
+def claim_device_post():
+    mac_address = request.form['mac_address']
+    customer_name = request.form['customer_name']
+    friendly_name = request.form['friendly_name']
+    location = request.form['location']
+
+    # Switch to the speedtest_db_clean database
+    client.switch_database(SPEEDTEST_DB)
+
+    # Add the claimed device to the devices measurement
+    json_body = [
+        {
+            "measurement": DEVICES_MEASUREMENT,
+            "tags": {
+                "tag_mac_address": mac_address
+            },
+            "fields": {
+                "field_customer_name": customer_name,
+                "field_friendly_name": friendly_name,
+                "field_location": location,
+                "field_mac_address": mac_address
+            }
+        }
+    ]
+    client.write_points(json_body)
+    return redirect(url_for('claim_device'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
